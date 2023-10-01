1bdebf22bb9d736b31d749781844c7cf2c5ad89d
VansRouges <evansagina57@gmail.com>
2023-10-02 00:49:34 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      serverActions: true,
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'assets.caisy.io',
+        },
+      ],
+  },}
+  
+  module.exports = nextConfig
